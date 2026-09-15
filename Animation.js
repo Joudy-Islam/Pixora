904685a2b0636818ad72d300cad65f7723d083e0
saloo00ma <saloo00ma@gmail.com>
2026-09-15 22:58:24 +0300

    Add Frame button

--- Animation.js
+++ Animation.js
@@ -1,15 +1,11 @@
-let frame = [];
-function handleUpload(event){
-const files = event.target.files
-frames= [];
+const addFrameBtn= document.getElementById("add");
+const frameInput = document.getElementById("frameinput");
+const framesContainer = document.getElementById("container");
+const  previewImage =document.getElementById("preview_image");
+const empteyMessage = document.getElementById("empty_message");
+let frame= [];
+let currentFrame = 0;
+addFrameBtn.addEventListener("click", function(){
+       frameInput.click();
+});
 
-for (let i =0; i< files.length ; i++){
-    const reader= new fileReader();
-    reader.onload= function(e){
-        frames.push(e.target.result);
-        displayFrames();
-    };
-    reader.ReadAsDataURL(files[i]);
-}
-
-}
